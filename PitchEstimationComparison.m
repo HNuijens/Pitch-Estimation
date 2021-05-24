cc5715a7ee2bf77b50e7caf69222169861d4c7fd
helmer <helmernuijens@gmail.com>
2021-05-24 15:10:43 +0200

    plot spectrogram and estimated pitch in one plot

--- PitchEstimationComparison.m
+++ PitchEstimationComparison.m
@@ -3,15 +3,16 @@ clear all;
 
 %% Load input signal
 [in, fs] = audioread("C:\Users\helme\Documents\Sound and Music Computing\SMC8\Sound and Music Signal Analysis\4. Slides and Exercises\Lecture 6 pitch estimation I\Exercises-20210407\09viola.flac");
-nHarmonics = 10;
-%% Create input signal
+nHarmonics = 5;
+
+%% Or create input signal
 fs = 44100;
 dur = 3;
 dt = 1/fs;
-t = 0:dt:dur-dt;
+t = (0:dt:dur-dt)';
 f = 200;
 nHarmonics = 10; 
-in = zeros(1,length(t));
+in = zeros(length(t),1);
 for h = 1:nHarmonics
     in = in + sin(2*pi*f*t*h);
 end
@@ -27,12 +28,12 @@ iVector = 1:nSegmentLength;
 nShift = round((1-overlap/100)*nSegmentLength);
 nSegments = ceil((nData-nSegmentLength+1)/nShift);
 
-pitch = zeros(nSegments,1);
+pitch = zeros(nData,1);
 
 %% Estimating pitch
 for i = 1:nSegments
     pitchVal =  PEHS(in(iVector), minFreq/fs, maxFreq/fs, nHarmonics);
-    pitch(i) = pitchVal*fs;
+    pitch(iVector) = pitchVal*fs;
     iVector = iVector + nShift;
 end
 
@@ -45,15 +46,11 @@ nDft = 4096;
 specNOverlap = round(3*specSegmentLength/4);
 [S, F, T] = spectrogram(in(:,1), specWindow, specNOverlap, nDft, fs);
 
-subplot(211);
-imagesc(T,F,20*log10(abs(S)))
+imagesc(T*fs,F,20*log10(abs(S)))
 set(gca,'YDir','normal')
-
+hold on
+plot(pitch,'r')
 ylim([0,1000])
 hold off
-xlabel('time [s]')
+xlabel('time [samples]')
 ylabel('frequency [Hz]')
-
-subplot(212)
-plot(pitch)
-ylim([0,1000]) 
