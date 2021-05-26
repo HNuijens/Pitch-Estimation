5e5b05704ada59386ca776c571d560f572a01221
helmer <helmernuijens@gmail.com>
2021-05-26 08:55:07 +0200

    cleaned code

--- PitchEstimationComparison.m
+++ PitchEstimationComparison.m
@@ -3,57 +3,58 @@ clear all;
 
 %% Load input signal
 [in, fs] = audioread("C:\Users\helme\Documents\Sound and Music Computing\SMC8\Sound and Music Signal Analysis\4. Slides and Exercises\Lecture 6 pitch estimation I\Exercises-20210407\09viola.flac");
-in = in(:,1);       % Get one channel
+in = in(:,1);                                       % Reduce to mono
 %% Or create input signal
-fs = 44100;
-dur = 3;
-dt = 1/fs;
-t = (0:dt:dur-dt)';
-f = 200;
-nHarmonics = 50; 
-freqTable = [200 400 500 633 233 980 120];
-nFrequencies = length(freqTable);
-in = zeros(length(t)*nFrequencies,1);
-indexVec = (1:length(t)); 
-for i = 1:nFrequencies
-    inSegment = zeros(length(t),1);
-    for h = 1:nHarmonics
+fs = 44100;                                         
+dur = 3;                                            % Duration of each frequency
+dt = 1/fs;                                          % Sample period
+t = (0:dt:dur-dt)';                                 % time vector                                             
+nHarmonics = 50;                                    % Amount of harmonics
+freqTable = [200 400 500 633 233 734 333];          % Frequency content of signal
+nFrequencies = length(freqTable);                   % Amount of frequencies
+in = zeros(length(t)*nFrequencies,1);               
+indexVec = (1:length(t));    
+
+for i = 1:nFrequencies                              % Repeat for each frequency
+    inSegment = zeros(length(t),1);                     
+    for h = 1:nHarmonics                            % Add harmonics to the signal
         inSegment = inSegment + (1/h)*sin(2*pi*freqTable(i)*t*h);
     end
-    in(indexVec) = inSegment;
+    in(indexVec) = inSegment;                       % Copy segment to the input signal
     indexVec = indexVec + length(t);   
 end
 
 %% Adding Noise
 nData = length(in);
-SNR = 0.6;                   % Desired signal / noise 
+SNR = 0.15;                  % Desired signal / noise 
 e = rand(nData,1)*2 - 1;     % Noise signal
 in = SNR*in + (1-SNR)*e;     % input signal plus added noise
 
 %% Function settings
-minFreq = 150;
-maxFreq = 1000;
-overlap = 75;
-nData = length(in);
-segmentLength =  25/1000;
-nSegmentLength = segmentLength * fs;
-iVector = 1:nSegmentLength;
-nShift = round((1-overlap/100)*nSegmentLength);
-nSegments = ceil((nData-nSegmentLength+1)/nShift);
-nHarmonics = 5;
-pitchAC = zeros(nData,1);
-pitchCF = zeros(nData,1);
-pitchHS = zeros(nData,1);
+minFreq = 150;                                      % Minimum frequency that can be estimated
+maxFreq = 1000;                                     % Maximum frequency that can be estimated
+overlap = 75;                                       % Percentage overlap from previous segment
+nData = length(in);                                 % Total amount of samples
+segmentLength =  25/1000;                           % Length of each segment in miliseconds
+nSegmentLength = segmentLength * fs;                % Segment Length in samples
+iVector = 1:nSegmentLength;                         % Vector used for indexing
+nShift = round((1-overlap/100)*nSegmentLength);     % Segment shift in samples
+nSegments = ceil((nData-nSegmentLength+1)/nShift);  % Total amount of segments                                   
+nHarmonic = 5;                                      % Amount of harmonics of analyzed signal
+
+pitchAC = zeros(nData,1);                           % Vector containing the estimated pitch using Auto Correlation
+pitchCF = zeros(nData,1);                           % Vector containing the estimated pitch using Comb Filtering
+pitchHS = zeros(nData,1);                           % Vector containing the estimated pitch using Harmonic Summation
 
 %% Estimating pitch
 for i = 1:nSegments
-    pitchAC(iVector) =  PEAC(in(iVector), minFreq/fs, maxFreq/fs);
-    pitchCF(iVector) =  PECF(in(iVector), minFreq/fs, maxFreq/fs);
-    pitchHS(iVector) =  PEHS(in(iVector), minFreq/fs, maxFreq/fs, nHarmonics);
+    pitchAC(iVector) =  PEAC(in(iVector), minFreq/fs, maxFreq/fs);              % Estimate pitch using Auto Correlation
+    pitchCF(iVector) =  PECF(in(iVector), minFreq/fs, maxFreq/fs);              % Estimate pitch using Comb Filtering
+    pitchHS(iVector) =  PEHS(in(iVector), minFreq/fs, maxFreq/fs, nHarmonics);  % Estimate pitch using Harmonic Summation
     
     iVector = iVector + nShift;
     
-    Text = ['Processing segment ',num2str(i), ' of ', num2str(nSegments)];
+    Text = ['Processing segment ',num2str(i), ' of ', num2str(nSegments)];      % Display current segment
     disp(Text)
 end
    pitchAC = pitchAC*fs;
